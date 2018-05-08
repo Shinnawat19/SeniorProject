98fe607b5d5ddbface64f8808665ddd8e6d397ca
thetongz <miiz_tongz@live.com>
2018-05-08 18:07:18 +0700

    add predict observation but waiting for all stocks

--- gym_stock/gym_stock/envs/stock_env.py
+++ gym_stock/gym_stock/envs/stock_env.py
@@ -1,7 +1,9 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+from keras.models import load_model
 import pandas as pd
+import numpy as np
 import os, json
 
 class StockEnv(gym.Env):
@@ -15,10 +17,13 @@ class StockEnv(gym.Env):
 		self.build_stock_market()
 		amount = 100
 
+	def load_model(self, model):
+		self.model = load_model(model + '.h5')
+
 	def build_stock_market(self):
 		self.sym = pd.read_csv("PTT.BK.csv")
-		self.i = 0
-		self.market = self.sym.iloc[[0]]
+		self.i = 60
+		self.market = self.sym.iloc[0: self.i]
 		self.balance = 1000000
 		self.equity = self.balance
 		self.portfolio = pd.DataFrame(None , columns = 
@@ -111,3 +116,52 @@ class StockEnv(gym.Env):
 		else:
 			print(self.portfolio.to_string())
 			print("\nCash " , self.balance , "     Volume " , self.portfolio['Volume'].sum() , "     Capital " , self.equity ,)
+
+	def getObservation(self):
+		'''
+			Load models -> get 30 days data to predict for 30 days
+
+			sample with one stock
+		'''
+		compared_moving_average = self.compared_with_moving_average()
+		
+		# waiting for 36 stocks
+
+		# predicts = self.predict_for_30_days(compared_moving_average)
+		# print(predicts)		
+
+	def compared_with_moving_average(self):
+		compared_moving_average = []
+		for i in range(30):
+			moving_average = (self.market[['Open', 'High', 'Low', 'Close']][i:30 + i].sum()/30).values.tolist()
+			current = self.market[['Open', 'High', 'Low', 'Close']][30 + i:30 + i + 1].values.tolist()
+			open = current[0][0] - moving_average[0]
+			high = current[0][1] - moving_average[1]
+			low = current[0][2] - moving_average[2]
+			close = current[0][3] - moving_average[3]
+			compared_moving_average.append([open, high, low, close])
+
+		return compared_moving_average
+
+	def predict_for_30_days(self, compared_moving_average):
+		predicts = []
+		test_data = np.asarray(compared_moving_average)
+		for index in range(30):
+			predict = self.model.predict(test_data, verbose = 0)
+			predicts.append(predict[0])
+			test_data = self.find_new_test_data(test_data, predict[0])
+
+		return predicts
+
+	def find_new_test_data(self, test_data, predict):
+		for (index, element) in enumerate(test_data[0]):
+			for i in range(len(element)):
+				if i == 0:
+					continue
+				elif i < 29:
+					element[i - 1] = element[i]
+				else:
+					element[i - 1] = element[i]
+					element[i] = predict[index]
+
+		return test_data
