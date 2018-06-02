3458f3205886bdc05ac3052a7ce153ae03902b88
taratep <taratep.sira@gmail.com>
2018-06-02 13:24:07 +0700

    fix ddpg and env, remove old model

--- gym_stock/gym_stock/envs/test_stock_env.py
+++ gym_stock/gym_stock/envs/test_stock_env.py
@@ -11,27 +11,26 @@ import time
 env = gym.make('stock-v0')
 
 #######################
-model = 'lstm'
-threshold = 15
-is_train = False
-is_Demo = False
+model = 'cnn'
+threshold = 30
+is_train = True
 epoch = 1000 if is_train else 1
 capital_all = []
 reward_all = []
+#######################
+
+env.load_model(model_name = model + str(threshold), 
+                is_train = is_train, 
+                threshold= threshold)
+
+#######################
 a_dim = env.actions.shape[0]
 a_bound = env.action_bound.high
 s_dim = env.observation_space.shape[0]
 #######################
 
-env.create_bot("bot03")
-if is_Demo:
-  time.sleep(3)
-env.load_model(model_name = model + str(threshold), 
-                is_train = is_train, 
-                threshold= threshold)
-env.set_demo(is_Demo)
 ddpg = DDPG(a_dim, s_dim, a_bound)
-ddpg.load_model('./ddpg_model/' + model + '_' + str(threshold) + '/')
+# ddpg.load_model('./ddpg_model/' + model + '_' + str(threshold) + '/')
 
 for i in range(epoch):
     s = env.reset()
@@ -42,7 +41,6 @@ for i in range(epoch):
 
         a = ddpg.choose_action(s)
         s_, r, done, info = env.step(a)
-        print('Day: ', day, ' Reward: ',ep_reward + r,' Capital: ', env.capital, 'Cash', env.balance ,'\n')
         if done:
             break
 
@@ -50,12 +48,11 @@ for i in range(epoch):
         s = s_
         ep_reward += r
         day += threshold
-        if is_Demo:
-          time.sleep(3)
-        
+    
+    print('Epochs:', i)
     print('Day: ', day, ' Reward: ',ep_reward + r,' Capital: ', env.capital,'\n')
     ddpg.learn()
     capital_all.append(env.capital)
     reward_all.append(reward_all)
 
-# ddpg.save_model('/' + model + '_' + str(threshold) + '/' + model)
+ddpg.save_model('/' + model + '_' + str(threshold) + '/' + model)
